0687c1f7913e3df048ad98e71017031b1c2082c4
Bobin <bobinpierreandre@gmail.com>
2025-07-09 14:57:20 +0200

    Creation d'EmployeController

---
+++ src/Controller/EntrepriseController.php
@@ -0,0 +1,36 @@
+<?php
+// Toutes les classes utilisées doivent etre importées
+namespace App\Controller;
+
+use App\Entity\Entreprise;
+use App\Repository\EntrepriseRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+// EntrepriseController qui hérite de la class AbstractController ce qui va permettre d'accéder 
+// à un certain nombre de méthode préétablie
+final class EntrepriseController extends AbstractController
+{
+    // Route et Name par défaut prend le nom du controller
+    // Name peut ete changer mais tjrs différent de l'ensbl des controller du projet
+    #[Route('/entreprise', name: 'app_entreprise')]
+    // Méthode index qui renvoie une response
+    // Response 
+   //1-  public function index(EntityManagerInterface $entityManager ): Response
+
+   //-> on va faire passer le repository tout de suite 
+    public function index(EntrepriseRepository $entrepriseRepository ): Response //2- 
+    {
+        // Render permet de faire le lien entre le controller et la vue
+
+       //1-   $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
+        $entreprises = $entrepriseRepository->findAll();  //2-
+        return $this->render('entreprise/index.html.twig', [
+           'entreprises' => $entreprises,
+        ]);
+    }
+}
+
+
