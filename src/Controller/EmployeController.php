47eac1146a17a11925cb42df2116fe6eff3b27db
Bobin <bobinpierreandre@gmail.com>
2025-07-15 14:31:00 +0200

    Ajout
    Ajout  dans la base de données  des forms complétés d'Entreprise et d'Employé

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Employe;
 use App\Form\EmployeType;
 use App\Repository\EmployeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -23,13 +24,21 @@ final class EmployeController extends AbstractController
         ]);
     }
     
-
     #[Route('/employe/new', name: 'new_employe')]
-    public function new(Request $request): Response
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $employe = new Employe();
 
         $form = $this->createForm(EmployeType::class, $employe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $employe = $form->getData();
+            $entityManager->persist($employe);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_employe');
+        }
 
         return $this->render('employe/new.html.twig', [
             'formAddEmploye' => $form,
--- src/Form/EmployeType.php
+++ src/Form/EmployeType.php
@@ -17,16 +17,51 @@ class EmployeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class)
-            ->add('prenom', TextType::class)
-            ->add('dateNaissance', DateType::class)
-            ->add('dateEmbauche', DateType::class)
-            ->add('ville', TextType::class)
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('prenom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('dateNaissance', DateType::class, [
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('dateEmbauche', DateType::class, [
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
+            ->add('ville', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+
             ->add('entreprise', EntityType::class, [
                 'class' => Entreprise::class,
                 'choice_label' => 'raisonSociale',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
             ])
-            ->add('valider', SubmitType::class)
+
+            ->add('valider', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
         ;
     }
 
